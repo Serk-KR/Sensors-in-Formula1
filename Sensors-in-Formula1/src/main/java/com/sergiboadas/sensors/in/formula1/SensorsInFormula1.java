2e2272a3bd5bcad60fe5606d145173c301fd4488
Serk-KR <35376173+Serk-KR@users.noreply.github.com>
2021-03-05 10:43:02 +0100

    Read all regular files (with extention) from resources folder + .gitignore modified

--- Sensors-in-Formula1/src/main/java/com/sergiboadas/sensors/in/formula1/SensorsInFormula1.java
+++ Sensors-in-Formula1/src/main/java/com/sergiboadas/sensors/in/formula1/SensorsInFormula1.java
@@ -5,17 +5,34 @@
  */
 package com.sergiboadas.sensors.in.formula1;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author Sergi
  */
 public class SensorsInFormula1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static List<String> getFilesNamesOfTheFolder(String resourcesPath) {
+        List<String> listNames = new LinkedList<>();
+        File folder = new File(resourcesPath);
+        File[] regularFiles = folder.listFiles(f -> f.isFile() && f.canRead() && f.getName().contains("."));
+        listNames = Arrays.asList(regularFiles)
+                .stream()
+                .map(File::getName)
+                .collect(Collectors.toList());
+        return listNames;
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Read all files of resources folder
+        String resourcesPath = "./resources/";
+        List<String> filesNames = getFilesNamesOfTheFolder(resourcesPath);
+         filesNames.forEach(name -> System.out.println("Name: " + name));
     }
     
 }
